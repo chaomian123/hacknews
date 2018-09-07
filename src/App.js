283b8391b9d80aedefe842cf844052744b7e41fc
chaomian123 <892284133@qq.com>
2018-09-08 04:02:01 +0800

    实现了数据通过api获取和SPA的大部分功能
    实现了数据通过api获取和SPA的大部分功能

--- src/App.js
+++ src/App.js
@@ -1,57 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const DEFAULT_QUERY = 'react'
+const DEFAULT_QUERY = 'redux'
 const PATH_BASE = 'https://hn.algolia.com/api/v1'
 const PATH_SEARCH = '/search'
 const PARAM_SEARCH = 'query='
-const PARAM_PAGE = 'page='
 
 
-
-
+//主界面的框体是使用的flex布局，这里是定义一下各框格占据的宽度
 const largeColumn = { width: '40%', };
 const midColumn = { width: '30%', };
 const smallColumn = { width: '10%', }
-// const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase())
+const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase())
 
 
 class App extends Component {
     constructor(props) {
         super(props)
-
         this.state = {
             result: null,
             searchTerm: DEFAULT_QUERY,
         }
         this.setSearchTopStories = this.setSearchTopStories.bind(this)
         this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this)
-        this.onSearchChange = this.onSearchChange.bind(this)
-        this.onSearchSubmit = this.onSearchSubmit.bind(this)
         this.onDismiss = this.onDismiss.bind(this)
+        this.onSearchChange = this.onSearchChange.bind(this)
+    }
 
-    }
-    onSearchSubmit(event) {
-        const { searchTerm }= this.state
-        this.fetchSearchTopStories(searchTerm)
-        event.preventDefault()
-    }
     setSearchTopStories(result) {
-        const { hits, page } = result
-        const oldHits = page !== 0
-            ? this.state.result.hits
-            : []
-        const updatedHits = [
-            ...oldHits,
-            ...hits
-        ]
-
-        this.setState({
-            result: {hits: updatedHits, page }
-        })
+        this.setState({ result })
     }
-    fetchSearchTopStories(searchTerm, page = 0) {
-        fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`)
+    //用fetch取代了ajax实现了对hacknews新闻api数据的获取，使用promise实现异步
+    fetchSearchTopStories(searchTerm) {
+        fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
             .then(response => response.json())
             .then(result => this.setSearchTopStories(result))
             .catch(e => e)
@@ -68,7 +49,6 @@ class App extends Component {
         })
     }
     onDismiss(id) {
-        // 这里是如果这个id不是点击的id则保留，如果是点击的id则过滤掉
         const updatedHits = this.state.result.hits.filter(item => item.objectID !== id)
         this.setState({
             result: { ...this.state.result, hits: updatedHits}
@@ -77,113 +57,104 @@ class App extends Component {
     render() {
         //让代码变得更加简短
         const { searchTerm, result} = this.state
-        const page = ( result && result.page ) || 0
-        // if (!result) { return null }
+        if (!result) { return null }
         return (
-        <div className="App">
-            <div className='page'>
-                <div className='interactions'>
-                    <Search
-                        value={searchTerm}
-                        onChange={this.onSearchChange}
-                        onSubmit={this.onSearchSubmit}
-                    >
-                        Search
-                    </Search>
-                    {result &&
-                         <Table
+            <div className="App">
+                <div className='page'>
+                    <div className='interactions'>
+                        <Search
+                            value={searchTerm}
+                            onChange={this.onSearchChange}
+                        >
+                            Search
+                        </Search>
+                        <Table
                             list={result.hits}
+                            pattern={searchTerm}
                             onDismiss={this.onDismiss}
                         />
-                    }
-                    <div className='interactions'>
-                        <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
-                            More
-                        </Button>
                     </div>
                 </div>
+
+
             </div>
-
-
-      </div>
-    )
-  }
+        )
+    }
 }
 
-// class ExplainBindingsComponent extends Component {
-//     constructor() {
-//         super()
-//         this.onClickMe = this.onClickMe.bind(this)
-//     }
-//     onClickMe() {
-//         console.log(this)
-//     }
-//     render() {
-//         return(
-//             <button
-//                 onClick={this.onClickMe}
-//                 type='button'
-//             >
-//                 Click Me
-//             </button>
-//         )
-//     }
-// }
+//暂未用到的按钮组件
+class ExplainBindingsComponent extends Component {
+    constructor() {
+        super()
+        this.onClickMe = this.onClickMe.bind(this)
+    }
+    onClickMe() {
+        console.log(this)
+    }
+    render() {
+        return(
+            <button
+                onClick={this.onClickMe}
+                type='button'
+            >
+                Click Me
+            </button>
+        )
+    }
+}
 
-const Table = ({ list, onDismiss }) => {
+//搜索结果表组件
+const Table = ({ list, pattern, onDismiss }) => {
     return (
         <div className='table'>
-            {list.map(item =>
-                    <div key={item.objectID} className='table-row'>
+            {list.filter(isSearched(pattern)).map(item =>
+                <div key={item.objectID} className='table-row'>
                       <span style={largeColumn}>
                           <a href={item.url} >{item.title}</a>
                       </span>
-                        <span style={midColumn}>{item.author}</span>
-                        <span style={smallColumn}>{item.num_comments}</span>
-                        <span style={smallColumn}>{item.points}</span>
-                        <span style={smallColumn}>
+                    <span style={midColumn}>{item.author}</span>
+                    <span style={smallColumn}>{item.num_comments}</span>
+                    <span style={smallColumn}>{item.points}</span>
+                    <span style={smallColumn}>
                             <Button className='button-inline'
-                                onClick={() => onDismiss(item.objectID)}
+                                    onClick={() => onDismiss(item.objectID)}
                             >
                                 不感兴趣
                             </Button>
                         </span>
-                        {/*<Delete_Button keys={item.objectID} onDismiss={onDismiss}/>*/}
-                    </div>
+                    {/*<Delete_Button keys={item.objectID} onDismiss={onDismiss}/>*/}
+                </div>
             )}
         </div>
-     )
+    )
 }
 
-const Search = ({ value, onChange, children, onSubmit }) => {
-        return(
-            <form onSubmit={onSubmit}>
-                <input
-                    type="text"
-                    value={value}
-                    onChange={onChange}
-                />
-                <button type="submit">
-                    {children}
+const Search = ({ value, onChange, children }) => {
+    return(
+        <form>
+            {children} <input
+            type="text"
+            value={value}
+            onChange={onChange}
+        />
+        </form>
+    )
+}
+
+
+
+const Delete_Button = ({ onDismiss, keys }) => {
+    return (
+        <span>
+                <button
+                    onClick={() => onDismiss(keys)}
+                    type='button'
+                >
+                    dismiss
                 </button>
-            </form>
-        )
-    }
-
-
-
-// const Delete_Button = ({ onDismiss, keys }) => {
-//         return (
-//             <span>
-//                 <button
-//                     onClick={() => onDismiss(keys)}
-//                     type='button'
-//                 >
-//                     dismiss
-//                 </button>
-//             </span>
-//         )
-// }
+            </span>
+    )
+}
 
 
 const Button =({ onClick, className, children }) => {
